9ece324d294caffc9a092eeb80a5f98bee803c9c
amazon <yadavpranshu877@gmail.com>
2023-11-27 08:27:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-Comm Project</h1>
+      <button>Normal button</button>
+      <Button>Bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
